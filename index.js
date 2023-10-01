c29dec4103cf8919cb20799d6e8efc22b131701d
mctola1 <119270609+mctola1@users.noreply.github.com>
2023-10-01 12:13:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-// Create a new Date object with the current date and time
-const currentDate = new Date();
+function age() {
+    var d1 = document.getElementById('date').value;
+    var m1 = document.getElementById('month').value;
+    var y1 = document.getElementById('year').value;
 
-// Get the current date in various formats
-const year = currentDate.getFullYear(); // 4-digit year
-const month = currentDate.getMonth() + 1; // Month (0-11, so add 1 for the actual month)
-const day = currentDate.getDate(); // Day of the month
+    var date = new Date();
+    var d2 = date.getDate();
+    var m2 = 1 + date.getMonth();
+    var y2 = date.getFullYear();
+    var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-console.log(`Current Date: ${year}-${month}-${day}`);
+    if(d1 > d2){
+    d2 = d2 + month[m2 - 1];
+    m2 = m2 - 1;
+    }
+    if(m1 > m2){
+    m2 = m2 + 12;
+    y2 = y2 - 1;
+    }
+    var d = d2 - d1;
+    var m = m2 - m1;
+    var y = y2 - y1;
+
+    document.getElementById('age').innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days';
+    }
